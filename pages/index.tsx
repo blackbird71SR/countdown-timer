b239500cfb762008497b7cbb22f057ba0299c71b
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2022-10-02 18:46:30 -0500

    Integrate components with HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,37 @@
-import type {NextPage} from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+import { TimerInput } from '../components/TimerInput';
+import { TimerContainer } from '../components/TimerContainer';
 
 const Home: NextPage = () => {
-  return(
-    <div className="flex min-h-screen flex-col items-center bg-[#1e1f29">
+  return (
+    <div className='flex min-h-screen flex-col items-center bg-[#1e1f29'>
       <Head>
         <title>Launch Countdown Timer</title>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
+
+      <Header message={message} />
+
+      <TimerContainer
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+      />
+
+      <TimerInput
+        value={newTime}
+        handleClick={handleClick}
+        handleChange={handleChange}
+      />
+
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
